664c1dd28e03c45e747cd399d645af6bbd17baf2
saruhanoymaci <saruhan.oymaci@hotmail.com>
2022-06-28 06:43:37 +0300

    filtering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,51 +3,58 @@ import Pokedex from "../pokedex.json"
 import "../scss/Card.scss"
 
 function Card() {
-    const [searchTerm, setSearchTerm] = useState('')
-    return (
-        <div className='container'>
-            <div className='d-flex justify-content-center mt-3'>
-                <input className="form-control" type="search" placeholder="Search" onChange={e => setSearchTerm(e.target.value)} />
-            </div>
-            <div className='row py-4'>
-                {
-                    Pokedex.pokemon.map(
-                        pokemon => {
-                            let pokemonType;
-                            if (pokemon.type[1] == null) {
-                                pokemonType = <h6>Type:{pokemon.type[0]}</h6>
-                            }
-                            else {
-                                pokemonType = <h6>Type:{pokemon.type[0]},{pokemon.type[1]}</h6>
-                            }
-                            return (
-                                <div className='col-lg-4 col-md-6 col-sm-12' key={pokemon.id}>
-                                    <div className='custom-card '>
-                                        <div className='d-flex'><img src={pokemon.img} width={90} height={90} alt={pokemon.name} />
-                                            <div>
-                                                <h4>{pokemon.name}</h4>
-                                                <h6>Height:{pokemon.height}</h6>
-                                                <h6>Weight:{pokemon.weight}</h6>
-                                                {pokemonType}
+  const [searchTerm, setSearchTerm] = useState('')
+  return (
+    <div className='container'>
+      <div className='d-flex justify-content-center mt-3'>
+        <input className="form-control" type="search" placeholder="Search" onChange={e => setSearchTerm(e.target.value)} />
+      </div>
+      <div className='row py-4'>
+        {
+          Pokedex.pokemon.filter(
+            pokemon => {
+              if (searchTerm == "") {
+                return (pokemon.name)
+              }
+              else if (pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return (pokemon.name)
+              }
+            }
+          ).map((pokemon) => {
+            let pokemonType;
+            if (pokemon.type[1] == null) {
+              pokemonType = <h6>Type:{pokemon.type[0]}</h6>
+            }
+            else {
+              pokemonType = <h6>Type:{pokemon.type[0]},{pokemon.type[1]}</h6>
+            }
+            return (
+              <div className='col-lg-4 col-md-6 col-sm-12' key={pokemon.id}>
+                <div className='custom-card '>
+                  <div className='d-flex'><img src={pokemon.img} width={90} height={90} alt={pokemon.name} />
+                    <div>
+                      <h4>{pokemon.name}</h4>
+                      <h6>Height:{pokemon.height}</h6>
+                      <h6>Weight:{pokemon.weight}</h6>
+                      {pokemonType}
 
-                                            </div>
-                                        </div>
-                                        <div className='d-flex justify-content-end mb-2 custom-button'>
-                                            <button className='btn btn-secondary'>
-                                                More...
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            )
-                        }
-                    )
-                }
-
-            </div>
-        </div>
-    )
+                    </div>
+                  </div>
+                  <div className='d-flex justify-content-end mb-2 custom-button'>
+                    <button className='btn btn-secondary'>
+                      More...
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
 }
+
+
 
 export default Card
